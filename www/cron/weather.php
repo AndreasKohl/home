73bc4de21ae15411431caa7fe7134d11ed6194d1
netzwerkwelten <a.kohl@kohl-service.de>
2016-02-13 16:21:06 +0000

    weather

--- www/cron/weather.php
+++ www/cron/weather.php
@@ -13,8 +13,8 @@
 */
 header('Content-Type: application/json');
 error_reporting(0);
-require_once '/home/www/config/dbconfig.inc.php';
-require_once '/home/www/cron/functions.inc.php';
+require_once '/home/pi/home/www/config/dbconfig.inc.php';
+require_once '/home/pi/home/www/cron/functions.inc.php';
 // Load Settings Data
 $settings = settings();
 // Set timezone and location data for Weather data
@@ -25,12 +25,12 @@ $api_key=$settings['weather_api'];
 // Get Weather Data per openweathermap api
 $weather_api = "http://api.openweathermap.org/data/2.5/weather?q=".$city.",".$land."&appid=".$api_key;
 $json = file_get_contents($weather_api);
-file_put_contents('/home/www/weather/weather.json', $json);
+file_put_contents('/home/pi/home/www/weather/weather.json', $json);
 // Get Weather forecast
 $forecast_api = "http://api.openweathermap.org/data/2.5/forecast?q=".$city.",".$land."&mode=json&appid=".$api_key;
 $json_forecast = file_get_contents($forecast_api);
-file_put_contents('/home/www/weather/forecast.json', $json_forecast);
-
+file_put_contents('/home/pi/home/www/weather/forecast.json', $json_forecast);
+echo '1';
 
 
 ?>
